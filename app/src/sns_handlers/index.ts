556b6be3b241e23ed80b507df53c0d776f835bf0
jungai <juworaphol125@gmail.com>
2021-12-19 15:07:21 +0700

    style: lint happy

--- app/src/sns_handlers/index.ts
+++ app/src/sns_handlers/index.ts
@@ -1,20 +1,20 @@
-import type { SNSEvent } from 'aws-lambda'
-import got from 'got'
-import { getDiscordWebHookUrl } from '../utils/get_env'
+import type { SNSEvent } from 'aws-lambda';
+import got from 'got';
+import { getDiscordWebHookUrl } from '../utils/get_env';
 
-// lambda notify with discord for sns 
+// lambda notify with discord for sns
 export const cloudWatchAlarmWithSns = async (event: SNSEvent) => {
-  try {
-    await got.post(getDiscordWebHookUrl(), {
-      json: {
-        embeds: [{
-          description: JSON.stringify(event, null, 2)
-        }]
-      }
-    })
-
-  } catch (error) {
-    throw new Error(`${error}`)
-  }
-}
-
+	try {
+		await got.post(getDiscordWebHookUrl(), {
+			json: {
+				embeds: [
+					{
+						description: JSON.stringify(event, null, 2),
+					},
+				],
+			},
+		});
+	} catch (error) {
+		throw new Error(`${error}`);
+	}
+};
